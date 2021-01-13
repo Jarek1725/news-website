5eed5e4dda5223bd653882bcdc5c7f493c13a319
novvac <novvac21@protonmail.com>
2021-01-14 00:27:03 +0100

    Revert "update base card"
    This reverts commit c4398b00aeb3b1f425bf55554d8ef1bdd9b1db4d.

--- client/src/components/base/Card.vue
+++ client/src/components/base/Card.vue
@@ -5,13 +5,6 @@
         v-bind="$attrs"
         v-on="$listeners"
     >
-        <div
-            class="py-2 px-3 mb-4 grey darken-4 white--text body-2 font-weight-bold"
-            v-if="hasHeaderSlot"
-        >
-            <slot name="header"/>
-        </div>
-
         <slot/>
     </v-card>
 </template>
@@ -19,11 +12,6 @@
 <script>
 export default {
     name: "BaseCard",
-    computed: {
-        hasHeaderSlot() {
-            return !!this.$slots['header'];
-        }
-    }
 }
 </script>
 
