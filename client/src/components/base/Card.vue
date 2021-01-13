c4398b00aeb3b1f425bf55554d8ef1bdd9b1db4d
novvac <novvac21@protonmail.com>
2021-01-14 00:15:52 +0100

    update base card

--- client/src/components/base/Card.vue
+++ client/src/components/base/Card.vue
@@ -5,6 +5,13 @@
         v-bind="$attrs"
         v-on="$listeners"
     >
+        <div
+            class="py-2 px-3 mb-4 grey darken-4 white--text body-2 font-weight-bold"
+            v-if="hasHeaderSlot"
+        >
+            <slot name="header"/>
+        </div>
+
         <slot/>
     </v-card>
 </template>
@@ -12,6 +19,11 @@
 <script>
 export default {
     name: "BaseCard",
+    computed: {
+        hasHeaderSlot() {
+            return !!this.$slots['header'];
+        }
+    }
 }
 </script>
 
