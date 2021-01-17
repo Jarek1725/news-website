5ffd3ab55105b7fd08ea8da3103ec312cf428c28
novvac <novvac21@protonmail.com>
2021-01-18 00:13:33 +0100

    widgets sticky & updated

--- client/src/components/core/View.vue
+++ client/src/components/core/View.vue
@@ -8,7 +8,7 @@
                     <router-view name="feeds"></router-view>
                 </v-col>
 
-                <v-col cols="12" class="pr-md-0 pl-md-0" :md="4">
+                <v-col cols="12" class="pr-md-0 pl-md-0" :md="4" v-sticky="stickyOptions">
                     <core-widgets/>
                 </v-col>
             </v-row>
@@ -23,6 +23,13 @@ export default {
     name: 'CoreView',
     components: {
         CoreWidgets,
+    },
+    data() {
+        return {
+            stickyOptions: {
+                topSpacing: 16
+            }
+        }
     }
 }
 </script>
